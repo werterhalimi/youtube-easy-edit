676436dbc16bba115f9446b4a51954ac55acdc62
Shai <shai.halimi@gmail.com>
2022-06-30 22:29:44 +0200

    remove useless response

--- background.js
+++ background.js
@@ -1,6 +1,5 @@
 chrome.runtime.onMessage.addListener((message, sender, sendResponse) => {
     if (message === 'open-options') {
         chrome.tabs.create({ 'url': 'chrome-extension://' + chrome.runtime.id+'/empty.html' });
-        sendResponse(user);
     }
   });
